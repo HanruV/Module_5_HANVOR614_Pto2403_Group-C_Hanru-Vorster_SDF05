9cee4af9dee41fab4bda0d0fbce5f2ff5734cea1
Hanru Vorster <hanru.vorster@gmail.com>
2024-04-17 11:25:56 +0200

    added hasBlackJack variable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let firstCard = 11
 let secondCard = 10
 let sum = firstCard + secondCard
+let hasBlackJack = false
 
 // win/lose logic
 
@@ -8,6 +9,10 @@ if (sum <= 20) {
     console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
 } else {
     console.log("You're out of the game! 😭")
 }
+
+// CASH OUT!
+console.log(hasBlackJack)
