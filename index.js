76aefa2c43b0a25e36d4c8ac4e0e1b5bc0d8c2ec
Hanru Vorster <hanru.vorster@gmail.com>
2024-04-18 11:26:38 +0200

    added to start game to diplay the sum on page

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// global variables
 let firstCard = 10
 let secondCard = 11
 let sum = firstCard + secondCard
@@ -8,7 +9,11 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 console.log(messageEl)
 
+let sumEl = document.getElementById("sum-el")
+
+// logic
 function startGame() {
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
